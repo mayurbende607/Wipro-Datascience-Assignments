c42f9fa771122d17d954f53d8c109e7421490983
mayurbende607 <mayurbende607@gmail.com>
2026-06-26 16:29:16 +0530

    Create positive_negative_zero.py

---
+++ python_fundamentals/Flow-Control-Statments/positive_negative_zero.py
@@ -0,0 +1,10 @@
+# Write a program to check if a given number is Positive, Negative, or Zero.
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
