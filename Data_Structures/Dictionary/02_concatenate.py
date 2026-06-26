98048eb1478fcd588226fb12473bc598fa387bd6
mayurbende607 <mayurbende607@gmail.com>
2026-06-26 18:42:26 +0530

    Add program to concatenate multiple dictionaries

---
+++ Data_Structures/Dictionary/02_concatenate.py
@@ -0,0 +1,13 @@
+# 2.Write a program to concatenate the following dictionaries to create a new one. 
+
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+
+new_dic = {}
+
+new_dic.update(dic1)
+new_dic.update(dic2)
+new_dic.update(dic3)
+
+print(new_dic)
