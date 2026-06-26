5fcc968a81d8a72eda21473d2580d2c802eea695
mayurbende607 <mayurbende607@gmail.com>
2026-06-26 17:04:32 +0530

    Create 08_sum_of_digit.py

---
+++ python_fundamentals/Flow-Control-Statments/08_sum_of_digit.py
@@ -0,0 +1,11 @@
+# 8.Write a program to print the sum of all the digits of a given number
+
+num = int(input("Enter a number: "))
+sum = 0
+
+while num > 0:
+    digit = num % 10
+    sum = sum + digit
+    num = num // 10
+
+print("Sum =", sum)
