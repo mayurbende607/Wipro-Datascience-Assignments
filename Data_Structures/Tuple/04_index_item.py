6d9a452c930d2924e212138334061759feeede6b
mayurbende607 <mayurbende607@gmail.com>
2026-06-26 18:55:35 +0530

    Add program to find index of an item in a tuple

---
+++ Data_Structures/Tuple/04_index_item.py
@@ -0,0 +1,9 @@
+# 4.Write a program to find the index of an item in a tuple
+
+t = (10, 20, 30, 40, 50)
+
+element = int(input("Enter element: "))
+
+index = t.index(element)
+
+print("Index =", index)
