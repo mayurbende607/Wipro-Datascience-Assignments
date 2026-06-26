fc4a7820ccf7f97ec3fbbb85c59777efc20fce59
mayurbende607 <mayurbende607@gmail.com>
2026-06-26 18:32:58 +0530

    Add remove item by index functionality
    Implement a program to remove an item from a specified index in a list.

---
+++ Data_Structures/List/07_remove.py
@@ -0,0 +1,9 @@
+# 7. Write a program to remove the item from a specified index in a list.
+
+numbers = [10, 20, 30, 40, 50]
+
+index = int(input("Enter index to remove: "))
+
+numbers.pop(index)
+
+print(numbers)
