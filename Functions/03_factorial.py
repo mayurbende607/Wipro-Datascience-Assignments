117df649084f1c35e12e445e22f1f2140215f426
mayurbende607 <mayurbende607@gmail.com>
2026-06-26 20:38:15 +0530

    Add factorial function with user input
    Implemented a function to calculate the factorial of a non-negative integer and added user input for testing.

---
+++ Functions/03_factorial.py
@@ -0,0 +1,15 @@
+# 3.Write a function to calculate and return the factorial of a number (a non-negative integer)
+
+
+def factorial(n):
+    fact = 1
+
+    for i in range(1, n + 1):
+        fact *= i
+
+    return fact
+
+
+num = int(input("Enter a number: "))
+
+print("Factorial =", factorial(num))
