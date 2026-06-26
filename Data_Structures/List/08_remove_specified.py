5c7217abe5cb4733c8a298ecbefaecff778bcd38
mayurbende607 <mayurbende607@gmail.com>
2026-06-26 18:36:05 +0530

    Create 08_remove_specified.py

---
+++ Data_Structures/List/08_remove_specified.py
@@ -0,0 +1,9 @@
+ # 8.Write a program to remove the first occurrence of a specified element from a list.
+
+numbers = [10, 20, 30, 20, 40]
+
+element = int(input("Enter element to remove: "))
+
+numbers.remove(element)
+
+print(numbers)
