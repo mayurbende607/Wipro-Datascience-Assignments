9ed83c6146c9388fdd8f6c6ee6f5b6afad92f196
mayurbende607 <mayurbende607@gmail.com>
2026-06-26 17:06:01 +0530

    Create 09_reverse_num.py

---
+++ python_fundamentals/Flow-Control-Statments/09_reverse_num.py
@@ -0,0 +1,11 @@
+# 9.Write a program to reverse a given number and print.
+
+num = int(input("Enter a number: "))
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print(reverse)
