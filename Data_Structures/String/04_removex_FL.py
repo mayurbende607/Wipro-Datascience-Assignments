4e5dd0ae5c06b0f4010069d5cfae2cde5f99c440
mayurbende607 <mayurbende607@gmail.com>
2026-06-26 19:40:43 +0530

    Create 04_removex_FL.py

---
+++ Data_Structures/String/04_removex_FL.py
@@ -0,0 +1,11 @@
+# 4.Given a string, if the first or last character is 'x', return the string without those 'x' character, else return the string unchanged.
+
+s = input("Enter a string: ")
+
+if s.startswith("x"):
+    s = s[1:]
+
+if s.endswith("x"):
+    s = s[:-1]
+
+print(s)
