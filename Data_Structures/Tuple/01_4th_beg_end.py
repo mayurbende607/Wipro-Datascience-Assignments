12d02669d83d2329496426c95fb8f939f5b80188
mayurbende607 <mayurbende607@gmail.com>
2026-06-26 18:51:19 +0530

    Create 01_4th_beg_end.py

---
+++ Data_Structures/Tuple/01_4th_beg_end.py
@@ -0,0 +1,6 @@
+# 1.Write a program to print the 4th element from first and 4th element from last in a tuple.
+
+t = (10, 20, 30, 40, 50, 60, 70, 80)
+
+print("4th element from first:", t[3])
+print("4th element from last:", t[-4])
